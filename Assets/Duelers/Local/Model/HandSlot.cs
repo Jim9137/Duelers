3e986bbf4d83478e02c265d9a94e0b0c3b3e178c
Jim9137 <jim9137@gmail.com>
2020-05-07 21:33:24 +0300

    Add events to handslot

--- Assets/Duelers/Local/Model/HandSlot.cs
+++ Assets/Duelers/Local/Model/HandSlot.cs
@@ -1,9 +1,36 @@
-﻿using UnityEngine;
+﻿using Duelers.Local.Controller;
+using System;
+using UnityEngine;
 using UnityEngine.UI;
 
-public class HandSlot : MonoBehaviour
+namespace Assets.Duelers.Local.Model
 {
-    [SerializeField] private Text manaCost;
+    public class HandSlot : MonoBehaviour
+    {
+        [SerializeField] private Text manaCost;
 
-    public void SetMana(string v) => manaCost.text = v;
+        public static event Action<HandSlot> OnMouseOverEvent = delegate { };
+        public static event Action<HandSlot> OnMouseExitEvent = delegate { };
+        public static event Action<HandSlot> OnMouseClickEvent = delegate { };
+
+        private UnitCard _card;
+        public UnitCard UnitCardInHand
+        {
+            get => _card;
+            set
+            {
+                value.transform.parent = transform;
+                value.transform.localPosition = Vector3.zero;
+                _card = value;
+            }
+        }
+
+
+        // TODO: add some fancy glowing things in the future
+        private void OnMouseEnter() => OnMouseOverEvent(this);
+        private void OnMouseExit() => OnMouseExitEvent(this);
+        private void OnMouseUpAsButton() => OnMouseClickEvent(this);
+
+        public void SetMana(string v) => manaCost.text = v;
+    }
 }
